ce21112e1022db18aad9a767c9d312e03c744802
Tucker Marsh <marsht21@hanover.edu>
2021-02-07 14:57:15 -0500

    Display restaurant name in TempActivity and implement back buttons to delete all documents from collection when pressed.

--- app/src/main/java/com/marsht21/restaurantpicker/TempActivity.java
+++ app/src/main/java/com/marsht21/restaurantpicker/TempActivity.java
@@ -1,22 +1,107 @@
 package com.marsht21.restaurantpicker;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import org.w3c.dom.Text;
 
 public class TempActivity extends AppCompatActivity {
 
+    private static final String TAG = TempActivity.class.getSimpleName();
     private Toolbar toolbar;
+    private FirebaseFirestore db;
+    private FirebaseAuth mAuth;
+    private TextView restaurantName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_temp);
         toolbar = findViewById(R.id.toolbar_temp);
+        db = FirebaseFirestore.getInstance();
+        mAuth = FirebaseAuth.getInstance();
+        restaurantName = findViewById(R.id.restaurantName);
+
         setToolbar();
+
+        db.collection("restaurants")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                restaurantName.setText(document.get("name").toString());
+                                break;
+                                //Log.d(TAG, document.getId() + " => " + document.getData());
+                            }
+                        } else {
+                            Log.d(TAG, "Error getting documents: ", task.getException());
+                        }
+                    }
+                });
+
     }
+
+    @Override
+    public void onBackPressed() {
+        deleteRestaurantDocuments();
+
+        super.onBackPressed();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                deleteRestaurantDocuments();
+
+                Intent intent = new Intent(TempActivity.this, HomeActivity.class);
+                startActivity(intent);
+                finish();
+                break;
+        }
+        return true;
+    }
+
+    private void deleteRestaurantDocuments() {
+        db.collection("restaurants")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                Log.d(TAG, document.getId() + " => " + document.getData());
+                                db.collection("restaurants").document(document.getId()).delete();
+                            }
+                        } else {
+                            Log.d(TAG, "Error deleting documents: ", task.getException());
+                        }
+                    }
+                });
+    }
+
 
     private void setToolbar() {
         setSupportActionBar(toolbar);
